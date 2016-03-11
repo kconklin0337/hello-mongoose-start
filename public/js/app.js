b2c12e8b16de430aae2e6647888e37ce3ed3a8cf
Justin Dennison <justin1dennison@gmail.com>
2016-03-10 20:32:39 -0500

    stuff happened

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,24 @@ angular.module('todoApp', [
 
 ]); //this is the setter syntax, you are creating an application using this one
 
-
 angular.module('todoApp') //this is the getter syntax, we can use this so we don't have to make a variable to store our angular application; YAYYY no globals
       .controller('TodoController', TodoController);
 
-TodoContoller.$inject = ['$scope', '$http'];
+TodoController.$inject = ['$scope', '$http'];
 
 function TodoController($scope, $http){
+  $scope.todos = [];
+  initTodos();
 
+
+  function initTodos(){
+    $http.get('/api/todos')
+        .then(function(response){
+          console.log(response);
+          $scope.todos = response.data;
+        })
+        .catch(function(err){
+          console.err(err);
+        });
+  }
 }
